084866b6f84f16a0197bf6d638cb96c83305cfdc
huyouming <13027196096@163.com>
2017-08-14 20:12:39 +0800

    ..

--- sendNum.js
+++ sendNum.js
@@ -1,40 +1,43 @@
-
-"use strict";
-
-var SendNum = function(options){},clickable = true,timeStart = time;
-
-
-
-SendNum.prototype = {
-	"click" : function(){
-		$(document).on('click',options.ele,function(){
-			if(clickable){
+;(function($,window,document,undefined){
+	"use strict";
+	var SendNum = function(options){
+		this.default = {
+			time : 10,
+			clickable : true,
+			timeStart : options.time?options.time:10
+		}
+		this.settings = $.extend({},this.default,{
+			ele : options.ele,
+			time : options.time,
+			func : options.func
+		});
+		this.init()
+	}
+	SendNum.prototype.click = function(){
+		var __this = this;
+		$(document).on('click',__this.settings.ele,function(){
+			if(__this.settings.clickable){
 				var that = this,timer = setInterval(loop,1000);
 				function loop(){
-					options.time--;
-					if(options.time>0){
-						$(that).val()?$(that).val(options.time+'s之后再次发送'):$(that).text(options.time+'s之后再次发送');
+					__this.settings.time--;
+					if(__this.settings.time>0){
+						$(that).val()?$(that).val(__this.settings.time+'s之后再次发送'):$(that).text(__this.settings.time+'s之后再次发送');
 						$(that).attr('disabled', 'true');
-						clickable = false;
-					}else if(options.time==0){
+						__this.settings.clickable = false;
+					}else if(__this.settings.time==0){
 						$(that).val()?$(that).val('发送验证码'):$(that).text('发送验证码');
 						$(that).removeAttr('disabled');
-						clickable = true;
-						options.time = timeStart;
-						options.func && options.func();
+						__this.settings.clickable = true;
+						__this.settings.time = __this.settings.timeStart;
+						__this.settings.func && __this.settings.func();
 						clearInterval(timer);
 					}
 				}
 			}
 		})
-	},
-	"init" : function(){
-		var options = $.extend({
-			time : 60
-		},options);
+	};
+	SendNum.prototype.init = function(){
 		this.click()
 	}
-}
-
-SendNum.init(options)	
-
+	window["SendNum"]=SendNum
+})(jQuery,window,document);
